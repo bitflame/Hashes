a8f6f2ba9d6a501f117e5c460224c17fc601ae19
shahin ansari <sansari1@cox.net>
2021-10-15 17:13:06 -0400

    Need to delete() and keys()

--- src/main/java/CuckooHashTable.java
+++ src/main/java/CuckooHashTable.java
@@ -1,5 +1,11 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class CuckooHashTable<Key, Value> {
-    private class Entry{
+    private class Entry {
         Key key;
         Value value;
 
@@ -8,18 +14,20 @@ public class CuckooHashTable<Key, Value> {
             this.value = value;
         }
     }
+
     private final class HashFunction<T> {
         private final int mA, mB;
         private final int mLgSize;
 
-        public HashFunction(int coefficientA, int coefficientB, int lgSize){
+        public HashFunction(int coefficientA, int coefficientB, int lgSize) {
             mA = coefficientA;
             mB = coefficientB;
             mLgSize = lgSize;
         }
+
         public int hash(Key key) {
             /*if object is null just evaluate to zero*/
-            if (key==null) {
+            if (key == null) {
                 return 0;
             }
             /* otherwise split its hash code into upper and lower bits */
@@ -27,8 +35,183 @@ public class CuckooHashTable<Key, Value> {
             final int upper = hashCode >>> 16;
             final int lower = hashCode & (0xFFFF);
             /* return the pairwise product of those bits, shifted down so that
-            * only lgSize bits remain in the output */
-            return (upper * mA + lower * mB)>>>(32 - mLgSize);
+             * only lgSize bits remain in the output */
+            return (upper * mA + lower * mB) >>> (32 - mLgSize);
         }
     }
+
+    private int keysSize;
+    private int size;
+
+    private Entry keysAndValues[][];
+    private HashFunction[] hashFunctions;
+
+    CuckooHashTable(int size) {
+        this.size = size;
+        keysAndValues = (Entry[][]) Array.newInstance(Entry.class, 2, size);
+        //The lg of the hash table size
+        //used to distribute keys uniformly in the hash function
+        int lgM = (int) (Math.log(size) / Math.log(2));
+
+        hashFunctions = new HashFunction[2];
+        for (int i = 0; i < 2; i++) {
+            int randomCoefficientA = StdRandom.uniform(Integer.MAX_VALUE);
+            int randomCoefficientB = StdRandom.uniform(Integer.MAX_VALUE);
+            hashFunctions[i] = new HashFunction(randomCoefficientA, randomCoefficientB, lgM);
+        }
+    }
+
+    public boolean isEmpty() {
+        return keysSize == 0;
+    }
+
+    public int keysSize() {
+        return keysSize;
+    }
+
+    private void updateHashFunctions() {
+        int lgM = (int) (Math.log(size) / Math.log(2));
+        for (int i = 0; i < 2; i++) {
+            int randomCoefficientA = StdRandom.uniform(Integer.MAX_VALUE);
+            int randomCoefficientB = StdRandom.uniform(Integer.MAX_VALUE);
+
+            hashFunctions[i] = new HashFunction(randomCoefficientA, randomCoefficientB, lgM);
+        }
+    }
+
+    private void resize(int newSize) {
+        StdOut.println("New Size: " + newSize);
+
+        size = newSize;
+
+        Entry[][] oldEntries = keysAndValues;
+        keysAndValues = (Entry[][]) new Array.newInstance(Entry.class, 2, newSize);
+        boolean tryToResize = true;
+        while (tryToResize) {
+            tryToResize = false;
+            updateHashFunctions();
+            for (Entry[] keysAndValues : keysAndValues) {
+                Arrays.fill(keysAndValues, null);
+            }
+            //Try to add all keys and values - Hash table 1
+            for (Entry entry : oldEntries[0]) {
+                if (entry != null && tryToResize(entry) != null) {
+                    tryToResize = true;
+                    break;
+                }
+            }
+            //Hash table 2
+            if (!tryToResize) {
+                for (Entry entry : oldEntries[1]) {
+                    if (entry != null && tryToInsert(entry) != null) {
+                        tryToResize = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void rehash() {
+        StdOut.println("Rehashing keys: " + keysSize);
+        Entry[] tempKeysAndValues = (Entry[]) Array.newInstance(Entry.class, keysSize);
+        int tempKeysAndValuesIndex = 0;
+        for (int i = 0; i < 2; i++) {
+            for (Entry entry : keysAndValues[i]) {
+                if (entry != null) {
+                    tempKeysAndValues[tempKeysAndValuesIndex++] = entry;
+                }
+            }
+        }
+        boolean tryToRehash = true;
+
+        while (tryToRehash) {
+            tryToRehash = false;
+
+            updateHashFunctions();
+
+            for (Entry[] keysAndValues : keysAndValues) {
+                Arrays.fill(keysAndValues, null);
+            }
+            // Try to add all keys and values
+            for (Entry entry : tempKeysAndValues) {
+                if (tryToInsert(entry) != null) {
+                    tryToRehash = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Argument to get() cannot be null");
+        }
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Argument to get() cannot be null");
+        }
+        for (int hashTableIndex = 0; hashTableIndex < 2; hashTableIndex++) {
+            int hash = hashFunctions[hashTableIndex].hash(key);
+            if (keysAndValues[hashTableIndex][hash] != null && keysAndValues[hashTableIndex][hash].key.equals(key)){
+                return keysAndValues[hashTableIndex][hash].value;
+            }
+        }
+        return null;
+    }
+    public void put(Key key, Value value) {
+        if (key==null) {
+            throw new IllegalArgumentException("Key cannot be null");
+        }
+        if (value==null) {
+            delete(key);
+            return;
+        }
+        // Update key if it already exists
+        for (int hashTableIndex = 0; hashTableIndex < 2; hashTableIndex++) {
+            int hash = hashFunctions[hashTableIndex].hash(key);
+            if (keysAndValues[hashTableIndex][hash] != null && keysAndValues[hashTableIndex][hash].key.equals(key)){
+                keysAndValues[hashTableIndex][hash].value =value;
+                return;
+            }
+        }
+        //Key does not exist, let's insert it
+        // Check if the numer of keys is equal or more than alf of the hash size
+        if (keysSize >= size) {
+            resize(size*2);
+        }
+        Entry entry = new Entry(key, value);
+        while(entry != null) {
+            entry = tryToInsert(entry);
+            if (entry!=null) {
+                rehash();
+            }
+        }
+        keysSize++;
+    }
+    /*
+    * Given an Entry, tries to insert that entry into the hash table, taking several iterations if necessary
+    * @return The last Displaced entry or null if all collisions were resolved
+    * */
+    private Entry tryToInsert(Entry entry) {
+        int maxTries = size + 1;
+        int hashTableIndex = 0;
+        for (int numberOfTries = 0; numberOfTries < maxTries; numberOfTries++) {
+            int hash = hashFunctions[hashTableIndex].hash(entry.key);
+
+            if (keysAndValues[hashTableIndex][hash]==null) {
+                keysAndValues[hashTableIndex][hash]=entry;
+                return null;
+            }
+            Entry entryToDisplace = keysAndValues[hashTableIndex][hash];
+            keysAndValues[hashTableIndex][hash]=entry;
+            entry = entryToDisplace;
+            hashTableIndex = (hashTableIndex+1) % 2;
+        }
+        return entry;
+    }
+    // Need to delete() and keys()
 }
