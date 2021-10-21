acddb01c151e50c34d7782d95c35b59f27501117
shahin ansari <sansari1@cox.net>
2021-10-21 16:00:57 -0400

    Decoding is working also as far as I can see

--- src/main/java/GfG.java
+++ src/main/java/GfG.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Accumulator;
+
 public class GfG {
     String encode(String str) {
         if (str.equals("") || str == null || str.length() == 0)
@@ -24,19 +26,60 @@ public class GfG {
         return sb.toString();
     }
 
+    // below code from Geeks for Geeks
+    void gfg_Encode(String str) {
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (i < n - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            // print character and its count
+            System.out.print(str.charAt(i));
+            System.out.println(count);
+        }
+    }
+
+    // This method breaks if the number is larger than 9 i.e. one digit
     private String decode(String str) {
         StringBuilder sb = new StringBuilder();
         int lo = 0;
         int hi = 1;
-        while(hi<str.length()) {
+        while (hi < str.length()) {
             char letter = str.charAt(lo);
             int digit = Integer.parseInt(str.substring(hi, hi + 1));
             while (digit != 0) {
                 sb.append(letter);
                 digit--;
             }
-            lo = hi+1;
-            hi = lo+1;
+            lo = hi + 1;
+            hi = lo + 1;
+        }
+        return sb.toString();
+    }
+// this might be doable with a couple of pointers if we could use an array
+    private String decode_v2(String str) {
+        StringBuilder sb = new StringBuilder();
+        char c = str.charAt(0);
+        char currentChar;
+        int digits = 0;
+        for (int i = 0; i < str.length(); i++) {
+            currentChar = str.charAt(i);
+            if (!Character.isDigit(currentChar)) {
+
+                for (int j = 0; j < digits; j++) {
+                    sb.append(c);
+                }
+                digits=0;
+                c = currentChar;
+            }
+            if (Character.isDigit(currentChar) ) {
+                digits = digits * 10 + Integer.parseInt("" + currentChar);
+            }
+        }
+        for (int i = 0; i < digits; i++) {
+            sb.append(c);
         }
         return sb.toString();
     }
@@ -45,6 +88,9 @@ public class GfG {
         GfG G = new GfG();
         System.out.println(G.encode("a"));
         System.out.println(G.encode("aaaabbbccc"));
-        System.out.printf(G.decode("A3B1C2D1E1"));
+        System.out.println(G.decode_v2("A3B1C2D1E1"));
+        System.out.printf("\n%s\n", G.decode_v2("A10"));
+        System.out.printf("%s\n", G.decode_v2("A122"));
+        //System.out.printf("%s\n", G.decode_v2("A1232"));
     }
 }
